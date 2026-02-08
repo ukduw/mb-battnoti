f6a0e026677d03d40043b32ece64111a4d68390c
ukduw <edhkmayo@gmail.com>
2026-02-08 17:11:40 +0000

    added shebang+bash, BATTERY variable, notification if <= 20, notes on filepath/executable

--- battnoti.sh
+++ battnoti.sh
@@ -6,3 +6,14 @@
 # battery info - pmset, e.g. pmset -g batt
 # noti - osascript
 
+#!/bin/bash
+
+BATTERY=$(pmset -g batt | grep -o "[0-9]\+%" | tr -d '%')
+
+if [ "$BATTERY" -le 20 ]; then
+    osascript -e 'display notification "Battery is at '"$BATTERY"'%, buster" with title "Chaa deen la..."'
+fi
+
+
+# put in ~/bin/battnoti.sh
+# chmod +x ~/bin/battnoti.sh
