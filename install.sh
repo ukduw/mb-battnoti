f1a37f63cfeaab43a538714f07615b2a3be4a0a4
ukduw <edhkmayo@gmail.com>
2026-02-08 18:19:18 +0000

    added variables...

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+SCRIPT_NAME="battnoti.sh"
+SERVICE_NAME="com.mb.battnoti"
+
+SCRIPT_SRC="$(cd "$(dirname "$0")" && pwd)/$SCRIPT_NAME"
+PLIST_SRC="$(cd "(dirname "$0")" && pwd)/$SERVICE_NAME"
+
+SCRIPT_DEST="/usr/local/bin/$SCRIPT_NAME"
+PLIST_DEST="$HOME/Library/LaunchAgents/$SERVICE_NAME.plist"
+
+
