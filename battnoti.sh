226d23c025fd3167bc4cb26918d0f201fd7b6168
ukduw <edhkmayo@gmail.com>
2026-02-08 17:05:11 +0000

    pseudocode/notes bash script placeholder

---
+++ battnoti.sh
@@ -0,0 +1,8 @@
+# mac - launchd + LaunchAgent, not systemd
+    # write script
+    # create .plist file
+    # load it with launchctl
+
+# battery info - pmset, e.g. pmset -g batt
+# noti - osascript
+
