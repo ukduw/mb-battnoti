d0fd48a4dfe5eed39e3e825640ed08d8b48b63e5
ukduw <edhkmayo@gmail.com>
2026-02-14 12:59:39 +0000

    added conditions comments, written python style...

--- battnoti/battnoti.sh
+++ battnoti/battnoti.sh
@@ -5,12 +5,12 @@ STATE_FILE10="/temp/low_battnoti10"
 
 BATTERY=$(pmset -g batt | grep -o "[0-9]\+%" | tr -d '%')
 
-if [ "$BATTERY" -le 20 ] && [ "$BATTERY" -gt 10 ]; then
+if [ "$BATTERY" -le 20 ] && [ "$BATTERY" -gt 10 ]; then     # 20 >= BATTERY > 10
     if [ ! -f "$STATE_FILE20" ]; then
         osascript -e 'display notification "Battery is at '"$BATTERY"'%, buster" with title "🪫 Chaa deen la..."'
         touch "$STATE_FILE20"
     fi
-elif [ "$BATTERY" -le 10 ]; then
+elif [ "$BATTERY" -le 10 ]; then    # 10 >= BATTERY
     if [ ! -f "$STATE_FILE10" ]; then
         osascript -e 'display notification "⚠️ BATTERY AT '"$BATTERY"'% ⚠️" with title "🪫 CHAA DEEN AH!"'
         touch "$STATE_FILE10"
