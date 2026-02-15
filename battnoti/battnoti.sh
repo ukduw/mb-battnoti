6aba635f4364e633697d634e580519a45f2fea6e
ukduw <edhkmayo@gmail.com>
2026-02-15 18:02:32 +0000

    moved comments to readme placeholder

--- battnoti/battnoti.sh
+++ battnoti/battnoti.sh
@@ -20,22 +20,3 @@ else
     rm -f "$STATE_FILE10"
 fi
 
-
-# battery info - pmset, e.g. pmset -g batt
-# noti - osascript
-
-# mac - launchd + LaunchAgent, not systemd
-    # write script
-    # create .plist file
-    # load it with launchctl
-
-# bash script
-    # put in usr/local/bin/battnoti.sh
-    # chmod +x usr/local/bin/battnoti.sh
-
-# plist
-    # put in ~/Library/LaunchAgents/com.mb.battnoti.plist
-    # launchctl load ~/Library/LaunchAgents/com.mb.battnoti.plist
-    # launchctl unload ~/Library/LaunchAgents/com.mb.battnoti.plist
-
-    # load deprecated...? use bootstrap
